60fad8752c3db6bd8042b90b06b0be95e199476f
Ivan Willian <38171332+Mlklindoo@users.noreply.github.com>
2018-12-20 18:53:14 -0200

    Inserindo fundo na home e footer
    A imagem de fundo da Home foi inserida com responsivo, o footer também.

---
+++ HomePage/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHPriceSearch</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./Style/homeStyle.css"> <!--Chamada do CSS -->
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" /><!--Chamada da Open Sans-->
+</head>
+
+<body>
+	<container> <!-- Home Page Container -->
+		<div  class="homeFundo">
+		</div>
+	</container>
+
+	<footer class="footer">
+		<h3>© 2019 Casal Sensa. Todos os direitos não reservados.</h3>
+	</footer>
+</body>
+</html>
